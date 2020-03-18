052cebe0784a7887712d6ed4beca8d0e3a80e6cc
Kratos <desmond8500@gmail.com>
2020-03-18 20:14:55 +0000

    save

--- resources/views/0/index.blade.php
+++ resources/views/0/index.blade.php
@@ -1,6 +1,11 @@
 @extends('0.index.layout')
 
 @section('content')
+    <style>
+        .test{
+            background-color: lightcyan;
+        }
+    </style>
     <div class="row">
         <div class="col-sm-3">
             @include('0.content.menur')
@@ -61,6 +66,28 @@
                 </div>
                 <div class="col-md-3">
                     @include('0.content.menur')
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="card card-body bg-light">
+                <h3>Les produits les plus demandés</h3>
+                <div class="row">
+                    @foreach ($articles as $key => $article)
+                        <div class="col-md-3 mb-2">
+                            <div class="card">
+                                <img src="{{ asset($article) }}" class="card-img-top" alt="{{ asset($article) }}" height="150px">
+                                <div class="card-body">
+                                    <h5 class="card-title">Plastique</h5>
+                                    {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+                                    <a href="#" class="btn btn-primary btn-sm">Acheter</a>
+                                </div>
+                            </div>
+                        </div>
+                        @if ($key ==3)
+                            @break;
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
---
+++ resources/views/0/index/footer.blade.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-md-12">
+        <h3>Informations</h3>
+    </div>
+    <div class="col-md-5">
+        Adresse : Guédiawaye, Dakar/ Sénégal <br>
+        Appelez-nous : 77 555 76 37 / 77 728 88 95 <br>
+        Email : contact@senecolomarket.com
+    </div>
+    <div class="col-md-7">
+        <form action="">
+            @csrf
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Prénom">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Nom">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Adresse mail">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Téléphone">
+            </div>
+            <button class="btn btn-success">Valider </button>
+        </form>
+    </div>
+</div>

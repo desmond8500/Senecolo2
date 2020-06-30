5d87a82ce1fd94d5bab03b81cd94ec6400a65fd4
Kratos <desmond8500@gmail.com>
2020-06-30 09:02:07 +0000

    modification de la couleur de la banière

--- resources/views/0 Eiser/index/banner.blade.php
+++ resources/views/0 Eiser/index/banner.blade.php
@@ -1,11 +1,17 @@
 <style>
 .banner_area2{
-    background: #F6F6F6;
+    background: #0B7AED;
+    /* background: #F6F6F6; */
     padding-top: 10px;
     margin-bottom: 20px;
+    color: white;
 }
 .banner_link{
-    color: gray;
+    color: white;
+    /* color: gray; */
+}
+.banner_title{
+    color: white;
 }
 </style>
 
@@ -14,14 +20,14 @@
         <div class="container">
             <div class="banner_content d-md-flex justify-content-between align-items-center">
                 <div class="mb-3 mb-md-0">
-                    <h2>{{$banner->title ?? 'Banner title'}}</h2>
+                    <h2 class="banner_title">{{$banner->title ?? 'Banner title'}}</h2>
                     <p>{{$banner->message ?? 'Banner Message'}}</p>
                 </div>
                 <div>
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Trouver un produit" aria-label="Trouver un produit" aria-describedby="button-addon2">
                         <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Chercher</button>
+                            <button class="btn btn-outline-light" type="button" id="button-addon2">Chercher</button>
                         </div>
                     </div>
                 </div>
